3ed42271c41c875e72a8cd23e8f2c951d9cd7d63
Arnav Surve <arnavsurve@gmail.com>
2024-07-22 00:03:11 -0700

    feat: Add support for displaying formatted tracks in get_top_tracks and get_recent_tracks

--- lastfm.py
+++ lastfm.py
@@ -1,6 +1,8 @@
 import json
 import argparse
 import pylast
+import datetime
+import pytz
 
 # Load the API keys from the config file
 with open("config.json") as f:
@@ -23,24 +25,64 @@ lastfm_network = pylast.LastFMNetwork(
 
 def get_top_tracks(username, number):
     top_tracks = lastfm_network.get_user(username).get_top_tracks(period='7day', limit=number)
+    formatted_tracks = []
 
-    for i, track in enumerate(top_tracks):
-        print(f"{i+1} {track.item.title} by {track.item.artist.name} - {track.weight} scrobbles")
-    return top_tracks
+    for track in top_tracks:
+        formatted_tracks.append({
+            'title': track.item.title,
+            'artist': track.item.artist.name,
+            'play-count': track.weight
+        })
+    return formatted_tracks
 
 
 def get_recent_tracks(username, number):
     recent_tracks = lastfm_network.get_user(username).get_recent_tracks(limit=number)
+    formatted_tracks = []
 
-    for i, track in enumerate(recent_tracks):
-        printable = track_and_timestamp(track)
-        print(str(i + 1) + " " + printable)
-    return recent_tracks
+    for track in recent_tracks:
+        track_info = track_and_timestamp(track)
+        formatted_tracks.append({
+            'playback_date': track.playback_date,
+            'artist': track.track.artist.name,
+            'track': track.track.title,
+            'time_str': track_info['time_str']
+        })
+    print(formatted_tracks)
+    return formatted_tracks
 
 
 def track_and_timestamp(track):
-    return f"{track.playback_date}\t{track.track}"
+    playback_datetime = datetime.datetime.strptime(track.playback_date, "%d %b %Y, %H:%M")
+    playback_datetime = playback_datetime.replace(tzinfo=pytz.UTC)
 
+    # get current time in UTC
+    now_utc = datetime.datetime.now(pytz.UTC)
+
+    # calculate time difference
+    time_difference = now_utc - playback_datetime
+
+    # convert time difference to readable format
+    days, remainder = divmod(time_difference.total_seconds(), 86400)
+    hours, remainder = divmod(remainder, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    # format and return track information with time elapsed
+    if days > 0:
+        time_str = f"{int(days)} days, {int(hours)} hours ago"
+    elif hours > 0 and hours < 2:
+        time_str = f"{int(hours)} hour, {int(minutes)} minutes ago"
+    elif hours > 1:
+        time_str = f"{int(hours)} hours, {int(minutes)} minutes ago"
+    else:
+        time_str = f"{int(minutes)} minutes ago"
+
+    return {
+        'playback_date': playback_datetime,
+        'time_str': time_str,
+        'track': track.track.title,
+        'artist': track.track.artist.name
+    }
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
